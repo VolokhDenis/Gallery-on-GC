bb1c3f37898312fd01d53b90fb19a7537b695144
VolokhDenis <davoloh@gmail.com>
2016-08-01 21:31:45 +0300

    finall gallery commit

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,6 +2,17 @@
 goog.array.forEach(goog.dom.query('.pic-mini-a'), function(link) {
     goog.events.listen(link, 'click', function(funk){
         funk.preventDefault();
+        
         goog.dom.setProperties(goog.dom.$("pic-prev"), { src: this.href });
+run("pic-prev")
     }, false, link);
 });
+
+function run(el) {
+var elem = goog.dom.$(el);
+var queue = new goog.fx.AnimationSerialQueue();
+queue.add(new goog.fx.dom.Fade(elem, 0, 1, 600));
+queue.play();
+}
+
+
